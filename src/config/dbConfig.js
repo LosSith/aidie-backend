db17b39129feeebdd2f56a4caa429dadac2b7c62
David Quezada <david.quezada.m@gmail.com>
2024-10-05 14:55:16 -0300

    connection working

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -4,6 +4,12 @@ dotenv.config();
 
 const pool = new Pool({
     connectionString: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
 });
 
 const query = (text, params) => pool.query(text, params);
