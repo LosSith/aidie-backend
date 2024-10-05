43f9b5a21d02c856b205ef6bf5832e72dc8acf62
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-10-05 16:31:13 -0400

    add: vadation

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
 const usersController = require("../controllers/users.controller.js");
-
+const Validator = require("../validation/usersValidation.js");
 const router = Router();
-router.post("/register", usersController.createUser);
+router.post(
+  "/register",
+  Validator.UsersValidation.addValidator,
+
+  usersController.createUser
+);
 router.post("/login", usersController.postLogin);
 router.get("/", usersController.getUsers);
 router.post("/", usersController.createUser);
